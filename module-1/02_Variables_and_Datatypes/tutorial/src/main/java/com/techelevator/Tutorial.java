bbfed960b8c7ed5be7a0ff2172506d0bb997fcf0
Alex Sotkovsky <alexsotkovsky@live.com>
2022-05-17 10:06:14 -0400

    staging to pull

--- module-1/02_Variables_and_Datatypes/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/02_Variables_and_Datatypes/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -8,20 +8,18 @@ public class Tutorial {
         double costOfDinner = 120.00;
         int tipPercent = 18;
         int numberOfGuests = 4;
-
+        final double SALES_TAX_PERCENTAGE = 7.25;
+        double tip = costOfDinner * ((double)tipPercent / 100.00);
         // Greet the user
         System.out.println("********************************************");
         System.out.println("*** Welcome to the Restaurant Calculator ***");
         System.out.println("********************************************");
         System.out.println("Cost of dinner: $" + costOfDinner);
-
+        System.out.println("Sales Tax: $" + tip);
         /******************************************************************************/
         // Step 2: Calculate the sales tax and tip
         /******************************************************************************/
-
-
-
-
+        double salesTAX = costOfDinner * (SALES_TAX_PERCENTAGE / 100.00);
 
 
         /******************************************************************************/
@@ -29,19 +27,10 @@ public class Tutorial {
         /******************************************************************************/
 
 
-
-
-
-
         /******************************************************************************/
         // Step 4: Given the total number of dessert pieces, determine the number each
         //      guest gets, and the number left over after each guest eats their pieces.
         /******************************************************************************/
-
-
-
-
-
 
 
         System.out.println("********************************************");
