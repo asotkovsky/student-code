ac5a49b7f2a91bf170472e40d5c7d7c19227ccad
rachelle <rauh>
2022-05-20 11:01:47 -0400

    Lecture midpoint

--- module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/DiscountCalculator.java
+++ module-1/05_Command_Line_Programs/lecture-final/src/main/java/com/techelevator/DiscountCalculator.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 class DiscountCalculator {
 
 
@@ -9,12 +11,50 @@ class DiscountCalculator {
      * The program allows a user to set a discount amount in decimal form
      * and also allows the user to enter a series of prices, separated by spaces
      *
+     * Prices that end in .98 are special and are never discounted.
+     *
      * */
 
     /**
      * The main method is the start and end of our program
      */
     public static void main(String[] args) {
+        System.out.println("Welcome - yo whats up!?! yo yo yo");
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Please enter the discount amount to apply: (in decimal form - aka no %)");
+
+        String userInput = input.nextLine();
+
+        System.out.println("You entered: " + userInput);
+
+        double discount = Double.parseDouble(userInput);
+
+        System.out.println("Enter prices to be discounted, separated by spaces:");
+
+       String userSubmittedPrice = input.nextLine();
+
+       String[] pricesArray = userSubmittedPrice.split(" ");
+
+       double price = 0.0;
+
+       double[] dblPricesArray = new double[pricesArray.length];
+
+        System.out.println("Discounted price is: ");
+
+       for(int i = 0; i < pricesArray.length; i++){
+
+           dblPricesArray[i] = Double.parseDouble(pricesArray[i]);
+
+           double newPrice = dblPricesArray[i] * (1 - discount);
+
+           System.out.print(newPrice + " ");
+
+       }
+
+
 
     }
 
