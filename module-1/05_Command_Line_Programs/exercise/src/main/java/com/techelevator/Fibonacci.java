f9b925108153547b47168e0b2bdaf936f2e40fb4
Alex Sotkovsky <alexsotkovsky@live.com>
2022-05-20 16:52:17 -0400

    completed command line programs exercise 3

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,48 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+
+		// Welcome message
+
+		System.out.println("Welcome to the Fibonacci sequence generator! \n");
+
+		// Accept user input
+
+		System.out.println("Please enter a number:");
+		Scanner input = new Scanner(System.in);
+		String inputNumber = input.nextLine();
+
+		// Parse the string into a double
+
+		int intNumber = Integer.parseInt(inputNumber);
+
+		// Define the variables for the calculation
+
+		int num1 = 0;
+		int num2 = 1;
+
+		// Label the data we will be displaying
+
+		System.out.println("\nFibonacci sequence up to " + intNumber + ":");
+
+		// Start the sequence calculation. Referenced for guidance: https://www.geeksforgeeks.org/different-ways-to-print-fibonacci-series-in-java/
+
+		while (num1 <= intNumber){
+
+			System.out.print(num1 + " ");
+			int num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+		}
+
+		System.out.println("\n\nThank you for using our tool! \u263A ");
+
+
+
 
 	}
 
