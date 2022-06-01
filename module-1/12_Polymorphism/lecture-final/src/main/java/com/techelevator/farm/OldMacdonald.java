977de681ca011fcc392bd908bae85e5723a95d0f
Brian Lauvray <brian@techelevator.com>
2022-06-01 12:49:52 -0400

    Lecture code

--- module-1/12_Polymorphism/lecture-final/src/main/java/com/techelevator/farm/OldMacdonald.java
+++ module-1/12_Polymorphism/lecture-final/src/main/java/com/techelevator/farm/OldMacdonald.java
@@ -1,11 +1,16 @@
 package com.techelevator.farm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OldMacdonald {
 	public static void main(String[] args) {
 
-		FarmAnimal[] farmAnimals = new FarmAnimal[] { new Cow(), new Chicken() };
+		Singable[] singers = new Singable[] { new Cow(), new Chicken(), new Sheep(), new Tractor() };
+		FarmAnimal[] farmAnimals = new FarmAnimal[] { new Cow(), new Chicken(), new Sheep() };
 
-		for (FarmAnimal animal : farmAnimals) {
+		for (Singable animal : singers) {
 			String name = animal.getName();
 			String sound = animal.getSound();
 			System.out.println("Old MacDonald had a farm, ee, ay, ee, ay, oh!");
@@ -15,5 +20,50 @@ public class OldMacdonald {
 			System.out.println("Here a " + sound + " there a " + sound + " everywhere a " + sound + " " + sound);
 			System.out.println();
 		}
+
+		List<Sellable> itemsForSale = new ArrayList<Sellable>();
+
+		Sellable cow = new Cow();
+		itemsForSale.add( cow );
+
+		Sellable chicken = new Chicken();
+		itemsForSale.add( chicken );
+
+		Sellable sheep = new Sheep();
+		itemsForSale.add( sheep );
+
+		Sellable egg = new Egg();
+		itemsForSale.add( egg );
+
+		System.out.println();
+		System.out.println("----- FOR SALE -----");
+
+		for(Sellable animalForSale : itemsForSale) {
+			System.out.print(animalForSale.getName());
+			System.out.println(" : $" + animalForSale.getPrice());
+		}
+
+		MusicBox asMusicBox = new MusicBox();
+		asMusicBox.getName();
+		asMusicBox.getSound();
+		asMusicBox.getPrice();
+		asMusicBox.play();
+		asMusicBox.wind();
+
+		Singable asSingable = asMusicBox;
+		asSingable.getName();
+		asSingable.getSound();
+
+		Sellable asSellable = (Sellable) asSingable;
+		asSellable.getName();
+		asSellable.getPrice();
+
+		MusicBox backToMusicBox = (MusicBox) asSellable;
+		backToMusicBox.getName();
+		backToMusicBox.getSound();
+		backToMusicBox.getPrice();
+		backToMusicBox.wind();
+		backToMusicBox.play();
+
 	}
 }
